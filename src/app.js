f0d268e1d851b96cbd6a8072aeb409d047e04466
= <nikita.mishra.pri@gmail.com>
2026-05-31 00:19:43 +0530

    get and post updated

--- src/app.js
+++ src/app.js
@@ -25,10 +25,11 @@ app.post("/create-post", upload.single("image"), async (req,res)=>{
 
 app.get("/posts", async (req,res)=>{
     
-    
-    
+    const posts = await postModel.find()
+
     res.status(200).json({
-        message: "note fetched",
+        message: "post fetched",
+        posts
         
     })
 })
