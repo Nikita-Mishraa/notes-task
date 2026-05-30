710b12a96aaad78a6b96984187986478ab173989
= <nikita.mishra.pri@gmail.com>
2026-05-31 00:14:51 +0530

    get and post updated

--- src/app.js
+++ src/app.js
@@ -1,26 +1,39 @@
 const express = require ("express")
+const postModel = require("./models/post.model")
+const multer = require("multer")
+const uploadFile = require("./services/storage.service")
 
 const app = express()
-
 app.use(express.json())
-const notes = []
 
-app.post("/note", (req,res)=>{
-    notes.push(req.body)
+const upload = multer({storage:multer.memoryStorage()})
 
+app.post("/create-post", upload.single("image"), async (req,res)=>{
+    
+    const result = await uploadFile(req.file.buffer)
+    
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+    
     res.status(201).json({
-        message: "note created succesfully"
+        message: "Post created succesfully",
+        post
     })
 })
 
-app.get("/note", (req,res)=>{
+app.get("/posts", async (req,res)=>{
+    
+    
+    
     res.status(200).json({
         message: "note fetched",
-        notes: notes
+        
     })
 })
 app.delete("/note/:index", (req,res)=>{
-    const index = req.params.index
+    const id = req.params.index
     delete notes[index]
 
     res.status(200).json({
--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose")
 
 async function connectDB(){
-    await mongoose.connect(process.env.MONGO_URI)
-    
+    await mongoose.connect("mongodb://user1:GO8SAPHr7MpaR5vb@ac-myuzjzk-shard-00-00.vgr3ktj.mongodb.net:27017,ac-myuzjzk-shard-00-01.vgr3ktj.mongodb.net:27017,ac-myuzjzk-shard-00-02.vgr3ktj.mongodb.net:27017/?ssl=true&replicaSet=atlas-9uaeg4-shard-0&authSource=admin&appName=notes-task")
     console.log("connected to DB")
 }
 
