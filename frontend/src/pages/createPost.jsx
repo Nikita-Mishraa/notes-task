3288979e27387e41fd92c03fe4e8560579aa76fa
= <nikita.mishra.pri@gmail.com>
2026-06-03 23:36:06 +0530

    post creation

--- frontend/src/pages/createPost.jsx
+++ frontend/src/pages/createPost.jsx
@@ -7,7 +7,7 @@ const CreatePost = ()=>{
       <h1>Create Post</h1>
       <form>
         <input type="file" name = "image" accept = "image/*" />
-        <input type="text" name = "caption" required/>
+        <input type="text" name = "caption" placeholder = "Add Caption" required/>
         <button type="submit">Submit</button>
 
 
